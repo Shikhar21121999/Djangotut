66d7174924379ee161a30686a3f4415939e3fa8b
Shikhar Sharma <59838148+Shikhar21121999@users.noreply.github.com>
2020-12-31 19:55:15 +0530

    Update settings.py

--- djangotut/settings.py
+++ djangotut/settings.py
@@ -129,17 +129,11 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-# SMTP configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'shikharpanditji@gmail.com'
-# EMIAL_HOST_PASSWORD = 'pxgkwaxctrwnwykk'
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'shikharpanditji@gmail.com'
-EMAIL_HOST_PASSWORD = 'pxgkwaxctrwnwykk'
+EMAIL_HOST_USER = 'something@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
